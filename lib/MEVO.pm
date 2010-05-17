2a0d9e12ef877d210530733647be38f6df975e73
Shaun Ashby <Shaun.Ashby@unige.ch>
2010-05-17 16:30:39 +0200

    Prototype the call model..add pseudocode as start point

--- lib/MEVO.pm
+++ lib/MEVO.pm
@@ -1,8 +1,27 @@
+# NB: Contains some pseudo code:
 package MEVO;
 
 use warnings;
 use strict;
 
 our $VERSION = 0.01;
+
+use Sky::Model;
+
+# Flesh out the call model:
+sub handler() {
+    my $request = shift;
+    
+    my $result = Sky::Model->search('VO::Service::Cutout',{ POS     =>  [ split(",",$request->param('POS')) ],
+							    SIZE    =>  $request->param('SIZE'),
+							    FORMAT  =>  $request->param('FORMAT') });
+    # Return DECLINED or maybe a VO::Table::Error object:
+    if ($result->error) {
+	log("there was an error: ",$result->error);
+	return Apache::DECLINED;
+    }
+    
+    return $result;
+}
 
 1;
