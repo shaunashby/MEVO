cae7a10db4642dc22347e910d580388709c43986
Shaun Ashby <Shaun.Ashby@gmail.com>
2014-08-16 18:08:25 +0200

    Add placeholder functions to Sky::Model.

--- lib/Sky/Model.pm
+++ lib/Sky/Model.pm
@@ -3,6 +3,12 @@ package Sky::Model;
 use Moose;
 use namespace::clean -except => 'meta';
 
+sub search() {
+    my ($self,@args) = @_;
+}
+
+sub error() {}
+
 no Moose;
 
 __PACKAGE__->meta()->make_immutable();
